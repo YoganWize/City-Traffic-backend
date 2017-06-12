f68c05def6de3d1af184bf7dbb413b9d4a2586db
Pavel Karlovich <p.karlovich@gmail.com>
2017-06-12 17:30:11 +0300

    [+] mongoose; [+] draft of logger;

--- index.ts
+++ index.ts
@@ -1,36 +1,21 @@
-import * as http from 'http';
 import { appSettings } from './settings/';
 
+
 import { Server } from './modules/server';
+import { Logger } from './modules/logger';
+import { DAL } from './modules/dal';
+
 import * as restV1 from './REST/api/v1/index.rest';
 
 let apiServer = new Server(appSettings.port, restV1);
 apiServer.start();
+DAL.init();
 
+///////////
 
+let log = new DAL.LogModel();
+log.message = 'tst';
+log.date = new Date();
+log.save();
 
-
-/////////////////
-
-// import App from './App.class';
-//
-
-// const port = appSettings.port;
-// App.set('port', port);
-//
-// const server = http.createServer(App);
-// server.listen(port);
-// server.on('error', onError);
-// server.on('listening', () => console.log(`Listening on ${port}`) );
-//
-// function onError(error: NodeJS.ErrnoException): void {
-//     switch(error.code) {
-//         case 'EADDRINUSE':
-//             console.error(`${port} is already in use`);
-//             process.exit(1);
-//             break;
-//         default:
-//             throw error;
-//     }
-// }
 
